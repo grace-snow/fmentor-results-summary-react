540f2685dcb2c489331939320d097cd6d839552d
Grace Snow <grace.snow@mandogroup.com>
2023-04-18 17:29:29 +0100

    try out unfonts

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import Unfonts from "unplugin-fonts/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Unfonts({
+      google: {
+        families: [
+          {
+            name: "Hanken Grotesk",
+            styles: "wght@500;700;800",
+            defer: true,
+          },
+        ],
+      },
+    }),
+  ],
   assetsInclude: ["**/*.ttf"],
 });
